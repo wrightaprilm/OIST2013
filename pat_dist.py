3fce63450ad52322ea5f9bd4744cf841baabec18
April Wright <wright.aprilm@gmail.com>
2013-05-26 23:26:19 +0900

    Functional. "YAYAYYYYYYYYYYYY," she said, as if there was ever a doubt she could do this.

--- pat_dist.py
+++ pat_dist.py
@@ -5,6 +5,7 @@ import Bio
 import urllib2
 from Bio import Entrez
 import re
+import csv
 
 Entrez.email = 'wright.aprilm@gmail.com'
 
@@ -19,28 +20,41 @@ Entrez.email = 'wright.aprilm@gmail.com'
 
 
 def query_date_life():
-	handle = Entrez.efetch(db="taxonomy", id=sys.argv[1])	
-	record = Entrez.read(handle)
-	species1 = "Homo_sapiens"
-	species2= sys.argv[1]
-	url_string = "http://www.itis.gov/ITISWebService/services/ITISService/getScientificNameFromTSN?tsn="
-	newrl = url_string + species2
-	response = urllib2.urlopen(newrl)	
-	name_record = response.read()
-	split_cord = re.split('<', name_record)
-	for item in split_cord:
-		if re.search('combinedName>', item):
-			genus = re.findall('\w+ ', item)
-			species = re.findall(' \w+' , item)
-			if len(genus) == 1:
-				final_name = genus[0] + '_' + species[0]
-				final_name = final_name.replace(' ', '')
-	dl_string = "http://datelife.org/cgi-bin/R/result?input="
-	seps = "%2C"
-	capper = "&format=newickmed&partial=liberal&useembargoed=yes&uncertainty=100"
-	dl_query = dl_string + species1 + seps + final_name + capper
-	dl_response = urllib2.urlopen(dl_query)	
-	dl_read = response.read()
+	with open("ngram_results.csv", 'rw') as f: 
+		f_reader = csv.reader(f)
+		for row in f_reader:
+#			print row[0]
+#			handle = Entrez.efetch(db="taxonomy", id=row[0])	
+#			handle = Entrez.efetch(db="taxonomy", id=sys.argv[1])	
+#			record = Entrez.read(handle)
+			species1 = "Homo_sapiens"
+			species2= row[0]
+			url_string = "http://www.itis.gov/ITISWebService/services/ITISService/getScientificNameFromTSN?tsn="
+			newrl = url_string + species2
+			response = urllib2.urlopen(newrl)	
+			name_record = response.read()
+		#	print name_record
+			split_cord = re.split('<', name_record)
+			for item in split_cord:
+				if re.search('combinedName>', item):
+					genus = re.findall('\w+ ', item)
+					species = re.findall(' \w+' , item)
+					if len(genus) == 1:
+						final_name = genus[0] + '_' + species[0]
+						final_name = final_name.replace(' ', '')
+						dl_string = "http://datelife.org/cgi-bin/R/result?input="
+						seps = "%2C+"
+						capper = "&format=bestguess&partial=liberal&useembargoed=yes&uncertainty=100&usetnrs=yes&tnrssource=NCBI"
+						dl_query = dl_string + species1 + seps + final_name + capper
+						dl_response = urllib2.urlopen(dl_query)	
+						dl_read = dl_response.read()
+						print dl_read
+						with open("output.txt", 'w') as f: f.write(dl_response.read())
 
-		
+				else:
+					break
+			
+#			print dl_read
+#			print dl_query
+				
 query_date_life()	
